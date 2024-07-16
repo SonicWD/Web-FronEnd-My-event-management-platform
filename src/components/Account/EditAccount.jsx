33cdf2b8c149f7a374e86b6f6742990f77a5236d
LASM24 <luisalbertosanabriamendez@gmail.com>
2024-07-16 11:08:15 -0500

    edir acccount complete + updates

--- src/components/Account/EditAccount.jsx
+++ src/components/Account/EditAccount.jsx
@@ -1,49 +1,35 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { AuthContext } from '../../context/AuthContext';
 import '../../index.css';
+import { useNavigate } from 'react-router-dom';
 
-/**
- * El componente EditAccount permite a los usuarios editar la información de su cuenta.
- * Utiliza el estado para gestionar los inputs del formulario y envía una solicitud POST para actualizar los datos del usuario.
- *
- * Props:
- * - initialName: El nombre inicial del usuario (string, requerido).
- * - initialEmail: El correo electrónico inicial del usuario (string, requerido).
- * - initialPassword: La contraseña inicial del usuario (string, requerido).
- * - onSave: Función de devolución de llamada para manejar la acción de guardar (función, requerido).
- *
- * @param {Object} props - Propiedades del componente.
- * @returns {JSX.Element} El componente renderizado.
- */
-const EditAccount = ({ initialName, initialEmail, initialPassword, onSave }) => {
-  // Hooks de estado para manejar los inputs del formulario
+const EditAccount = ({ initialName = '', initialEmail = '', initialPassword = '', onSave }) => {
   const [name, setName] = useState(initialName);
   const [email, setEmail] = useState(initialEmail);
   const [password, setPassword] = useState(initialPassword);
+  const { authState } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  /**
-   * Maneja el envío del formulario para guardar los datos actualizados del usuario.
-   * Envía una solicitud POST al servidor con los datos actualizados.
-   *
-   * @param {Event} e - El evento de envío del formulario.
-   */
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    try {
-      // Envía una solicitud POST al servidor con los datos actualizados del usuario
-      const response = await axios.post('/api/updateUser', {
-        name,
-        email,
-        password
-      });
 
-      // Verifica si el estado de la respuesta es 200 (OK)
+    try {
+      const response = await axios.put(
+        'http://localhost:8000/updateUser', 
+        { username: name, email, password },
+        {
+          headers: {
+            Authorization: `Bearer ${authState.token}`
+          }
+        }
+      );
+
       if (response.status === 200) {
-        // Llama a la función onSave con los datos actualizados
         onSave({ name, email, password });
+        navigate('/login');
+        alert('Update info');
       } else {
         console.error('Error al actualizar la información del usuario:', response.data);
       }
@@ -84,11 +70,10 @@ const EditAccount = ({ initialName, initialEmail, initialPassword, onSave }) => 
   );
 };
 
-// PropTypes para verificar el tipo de las props
 EditAccount.propTypes = {
   initialName: PropTypes.string.isRequired,
   initialEmail: PropTypes.string.isRequired,
-  initialPassword: PropTypes.string.Optional,
+  initialPassword: PropTypes.string,
   onSave: PropTypes.func.isRequired,
 };
 
